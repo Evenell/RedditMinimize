26892a34ae4e6f7274abb8b946861f4b8a723203
Michael Song <Michaelsong0@gmail.com>
2018-07-31 15:24:31 -0400

    added for loop in js code

--- RedditMinimize/minimize.js
+++ RedditMinimize/minimize.js
@@ -1,9 +1,11 @@
 window.addEventListener('load', function() {
-    var parent = document.getElementsByClassName('s545dfq-0 uXALa')
+    var parent = document.getElementsByClassName("s545dfq-0 uXALa")
 
     var minimize = document.createElement("button");
     minimize.innerHTML = "old";
-    parent[0].appendChild(minimize);
+    for (int i = 0; i < parent.length; i++) {
+    	parent[i].appendChild(minimize);
+    }
 
     minimize.addEventListener("click", function() {
         "s545dfq-0 uXALa" {
