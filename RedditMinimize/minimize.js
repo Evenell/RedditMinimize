747c0d7d9aefc77a93c746dff6d943fdac39d9bc
Michael Song <Michaelsong0@gmail.com>
2018-07-31 00:09:06 -0700

    Add files via upload

---
+++ RedditMinimize/minimize.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function() {
+    var parent = document.getElementsByClassName('s545dfq-0 uXALa')
+
+    var minimize = document.createElement("button");
+    minimize.innerHTML = "old";
+    parent[0].appendChild(minimize);
+
+    minimize.addEventListener("click", function() {
+        "s545dfq-0 uXALa" {
+            display: none;
+        }
+    });
+});
