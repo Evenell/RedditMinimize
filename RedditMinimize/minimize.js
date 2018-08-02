9466ece0850fa1e1f792667b507654249225fbfc
Michael Song <micsong@seas.upenn.edu>
2018-08-02 15:08:44 -0700

    fixed infinite scroll issue and added comments

--- RedditMinimize/minimize.js
+++ RedditMinimize/minimize.js
@@ -2,21 +2,26 @@
  * Author: Michael Song
  * Last Edited: 8/1/2018
  * Description: JS code that allows you to minimize posts on reddit.com
- * TODO: Get it to update as reddit infinite scroll gives more posts. 
+ *  
  */
 
 var refresh = function() {
+    //gets all divs with the id of a post
     var parent = $('div[id^="t3_"]');
     var buttons = [];
     var bools = [];
     var i;
-    for (i = 0; i < parent.length; i++) {
+    //all the odd indexed divs are the share buttons, so we skip over those by incrementing by 2
+    for (i = 0; i < parent.length; i = i + 2) {
+
+        //use try catch because it wasn't working without it (seriously idk)
         try { throw i } catch (ii) {
             var grandparentDiv = parent[ii].parentNode;
+
+            //to prevent function from adding a new button each time
             if(grandparentDiv.firstChild.type == "button") {
                 continue;
             }
-            console.log(ii);
             bools[ii] = true;
             buttons[ii] = document.createElement("button");
             buttons[ii].setAttribute("type", "button");
@@ -26,22 +31,27 @@ var refresh = function() {
                 console.log(ii);
                 if (bools[ii] == true) {
                     $(parent[ii]).children().hide();
+
+                    /* Debugging
                     console.log("0 to 1 success");
                     console.log("1");
+                    */
                 } else if (bools[ii] == false) {
                     $(parent[ii]).children().show();
+
+                    /* Debugging
                     console.log("1 to 0 success");
                     console.log("0");
-
+                    */
                 }
                 bools[ii] = !bools[ii];
             });
-
-
             grandparentDiv.insertBefore(buttons[ii], parent[ii]);
 
         }
     }
 }
 
+//A way to ensure that all posts have the button as the user keeps scrolling
+window.addEventListener('scroll', function(){refresh();});
 refresh();
